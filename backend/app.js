601a26ba89347eb6350d4875d47a9ba03dbd5ff1
sanjid <sanjidvadakkan@gmail.com>
2025-07-03 16:00:46 +0530

    💄 UI updated for task page

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('✅ MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI,)
+.then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.log(err));
 
 var indexRouter = require('./routes/index');
